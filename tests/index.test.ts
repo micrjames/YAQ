d407de850f5f8f20b49ee98515ec6e89ed5ddbc3
Michael James <micrjames921@gmail.com>
2024-11-08 13:25:23 -0700

    completed tests on section 'dequeue

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -30,12 +30,8 @@ describe("An array-like Queue of data", () => {
 	  });
 	  describe("enqueue", () => {
 		 let enQValue: number;
-		 let enQidx: number;
-		 let errIdx: number;
 		 beforeAll(() => {
 			enQValue = 1;
-			enQidx = 0;
-			errIdx = -1;
 			q.enqueue(enQValue);
 			qSize++;
 		 });
@@ -45,13 +41,34 @@ describe("An array-like Queue of data", () => {
 		 });
 		 test("Should retrieve the element that was 'enqueue'd", () => {
 			const data = q.front();
-			console.log(`Initial enqueue: ${q.toString()}`);
+			console.log(`Initial enqueue: ${q.toString()}.`);
 			expect(data).toBe(enQValue);
 		 });
 		 test("Should be of a size of one element.", () => {
 			const size = q.size;
 			expect(size).toBe(qSize);
 		 });
+	  });
+	  describe("dequeue", () => {
+		 let qValue: number;
+		 let data: number;
+		 beforeAll(() => {
+			qValue = q.front();
+			q.dequeue();
+			qSize--;
+		 	data = q.front();
+		 });
+		 test("Should remove the element at the front of the queue.", () => {
+			console.log(`Dequeue'd value ${qValue} at 0th index gives queue: ${q.toString()}.`);
+			expect(data).not.toBe(qValue);
+		 });
+		 test("Should be an empty queue.", () => {
+			expect(data).toBeNull();
+		 });
+		 test("Should have a size of 0.", () => {
+			const size = q.size;
+			expect(size).toBe(qSize);
+		 });	
 	  });
    });
 });
